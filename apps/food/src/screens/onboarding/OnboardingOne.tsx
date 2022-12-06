6fa9a32424ee0847a77991888891dfb6177f1c91
tonisco <tonyumeozulu@gmail.com>
2022-12-06 22:19:39 +0000

    onboarding one, added heading, desc

--- apps/food/src/screens/onboarding/OnboardingOne.tsx
+++ apps/food/src/screens/onboarding/OnboardingOne.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, SafeAreaView } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const OnboardingOne = () => {
     return (
-        <View>
-            <Text>This is Onboarding one</Text>
-        </View>
+        <SafeAreaView className='justify-center items-center flex-1 p-3.5 gap-8 bg-color'>
+            <View className='justify-center items-center gap-y-5'>
+            <Image className='h-40 w-40' source={require('../../../assets/onboarding4.png')} />
+            <View className='bg-green-700'>
+                <Text className='text-4xl uppercase bg-clip-text'>Order a Tasty Dish</Text>
+            </View>
+            <Text className='text-xs text-dark'>Discover the best foods from over 1,000 restaurants and fast delivery to your doorstep</Text>
+            <View className='flex-row gap-x-3'>
+                <View className='h-2.5 w-2.5 bg-lite-green rounded-full'></View>
+                <View className='h-2.5 w-2.5 bg-lite-gray rounded-full'></View>
+                <View className='h-2.5 w-2.5 bg-lite-gray rounded-full'></View>
+            </View>
+            </View>
+            <TouchableOpacity className=''>
+                <Text>next</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
 
--- packages/tailwind-config/index.js
+++ packages/tailwind-config/index.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}", "./src/App.tsx"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  theme: {
+    extend: {},
+    colors: {
+      'lite-green': '#53E88B',
+      'dark-green': '#15BE77',
+      'lite-gray': '#D9D9D9',
+      'bg-color': '#F9F9F9',
+      'dark': '#3B3B3B'
+    }
+  },
+  plugins: [],
+}
