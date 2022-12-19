2eecae5f99199e7593d20277357e4da5b8e4ae80
tonisco <tonyumeozulu@gmail.com>
2022-12-19 22:39:26 +0100

    location form

--- apps/food/src/screens/auth/LocationFormScreen.tsx
+++ apps/food/src/screens/auth/LocationFormScreen.tsx
@@ -1,12 +1,136 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import {
+  BackButton,
+  GradientButton,
+  Input,
+  IsIos,
+  Colors,
+  TextSize,
+} from "mobile-ui"
 import React from "react"
-import { View, Text } from "react-native"
+import { useForm } from "react-hook-form"
+import { View, Text, StyleSheet, ScrollView } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import * as z from "zod"
 
-const LocationFormScreen = () => {
+import { AuthStack } from "../../constants/screen"
+
+type FormData = {
+  street: string
+  city: string
+  country: string
+}
+
+type Props = NativeStackScreenProps<AuthStack, "Location Form">
+
+const LocationFormScreen = ({ navigation }: Props) => {
+  const schema = z.object({
+    street: z.string(),
+    city: z.string(),
+    country: z.string(),
+  })
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { street: "", city: "", country: "" },
+    resolver: zodResolver(schema),
+  })
+
+  const createProfile = (data: FormData) => {
+    console.log(data)
+    console.log("Leaving Reset Password")
+    navigation.navigate("Location Form")
+  }
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scroll}
+        keyboardShouldPersistTaps="handled"
+      >
+        <BackButton />
+        <View style={styles.textContainer}>
+          <Text style={styles.heading}>Fill in your bio to get started</Text>
+          <Text style={styles.description}>
+            This data will be displayed in your account profile for security
+          </Text>
+        </View>
+        <Input
+          check
+          control={control}
+          inputName="street"
+          placeholder="Street Address"
+          iconName={IsIos ? "ios-location" : "location"}
+          style={{ width: "100%" }}
+          error={errors.street?.message}
+        />
+
+        <Input
+          check
+          control={control}
+          inputName="city"
+          placeholder="City"
+          iconName={IsIos ? "ios-location" : "location"}
+          style={{ width: "100%" }}
+          error={errors.city?.message}
+        />
+
+        <Input
+          check
+          control={control}
+          inputName="country"
+          placeholder="Country"
+          iconName={IsIos ? "ios-location" : "location"}
+          style={{ width: "100%" }}
+          error={errors.country?.message}
+        />
+
+        <View style={styles.buttonContainer}>
+          <GradientButton text="next" onPress={handleSubmit(createProfile)} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 25,
+    paddingHorizontal: 25,
+  },
+  scroll: {
+    flex: 1,
+  },
+  textContainer: {
+    alignItems: "flex-start",
+    marginBottom: 15,
+  },
+  heading: {
+    fontFamily: "font-bold",
+    fontSize: TextSize.large,
+    textTransform: "uppercase",
+    color: Colors.dark,
+    textAlign: "left",
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: TextSize.tiny,
+    color: Colors.dark,
+    textAlign: "left",
+    fontFamily: "font-regular",
+    lineHeight: 13,
+    marginTop: 3,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 50,
+    alignSelf: "center",
+  },
+})
 
 export default LocationFormScreen
