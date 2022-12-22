325e10b6f9b3042bd67e246fb60a8868795f109a
tonisco <tonyumeozulu@gmail.com>
2022-12-22 11:31:50 +0100

    expo eslint

--- apps/food/.eslintrc.js
+++ apps/food/.eslintrc.js
@@ -1,11 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   ...require("esconfig/native-eslint"),
-  parserOptions: {
-    // eslint-disable-next-line no-undef
-    tsconfigRootDir: __dirname,
-    project: "./tsconfig.json",
-  },
-  settings: {
-    "import/ignore": ["react-native"],
-  },
 }
--- packages/esconfig/native-eslint.js
+++ packages/esconfig/native-eslint.js
@@ -1,11 +1,22 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["universe", "universe/shared/typescript-analysis"],
+  extends: [
+    "../../.eslintrc.cjs",
+    "universe/native",
+    "universe/shared/typescript-analysis",
+    "@react-native-community",
+    "turbo",
+    "prettier",
+  ],
+  parserOptions: {
+    requireConfigFile: false,
+  },
   overrides: [
     {
       files: ["*.ts", "*.tsx", "*.d.ts"],
       parserOptions: {
         project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
       },
     },
   ],
