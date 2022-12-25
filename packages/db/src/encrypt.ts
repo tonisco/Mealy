a45fa2c1b283fc242594102416bc4e4db9d7a55a
tonisco <tonyumeozulu@gmail.com>
2022-12-25 21:51:56 +0100

    trpc transformer

--- packages/db/src/encrypt.ts
+++ packages/db/src/encrypt.ts
@@ -1,20 +1,23 @@
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt"
 
 export const encryptPassword = async (password: string) => {
-    let saltRounds = 10
-    try {
-        const salt = await bcrypt.genSalt(saltRounds)
-        const hashedPassword = await bcrypt.hash(password, salt)
-        return hashedPassword
-    } catch (error) {
-        throw new Error('Failed to hash password')
-    }
+  const saltRounds = 10
+  try {
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    return hashedPassword
+  } catch (error) {
+    throw new Error("Failed to hash password")
+  }
 }
 
-export const comparePasswords = async (password: string, hashedPassword: string) => {
-    try {
-        return await bcrypt.compare(password, hashedPassword)
-    } catch (error) {
-        throw new Error('Failed to compare password')
-    }
+export const comparePasswords = async (
+  password: string,
+  hashedPassword: string,
+) => {
+  try {
+    return await bcrypt.compare(password, hashedPassword)
+  } catch (error) {
+    throw new Error("Failed to compare password")
+  }
 }
--- packages/trpc-client/src/TrpcProvider.tsx
+++ packages/trpc-client/src/TrpcProvider.tsx
@@ -2,7 +2,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { httpBatchLink } from "@trpc/client"
 import React, { useState } from "react"
 
-import { trpc } from "."
+import { transformer } from "./transformer"
+import { trpc } from "./trpc"
 
 export function TrpcProvider({
   children,
@@ -15,6 +16,7 @@ export function TrpcProvider({
   const [queryClient] = useState(() => new QueryClient())
   const [trpcClient] = useState(() =>
     trpc.createClient({
+      transformer,
       links: [
         httpBatchLink({
           url: `${port}/api/trpc/`,
