b6b0a0840cf50c1ee701da51503d2d34b5e23b06
tonisco <tonyumeozulu@gmail.com>
2023-01-10 08:53:53 +0100

    images fix

--- packages/db/src/seed/data.ts
+++ packages/db/src/seed/data.ts
@@ -1,12 +1,36 @@
 export const restaurantData = [
-  { name: "Eatery works", logo: "/logo/logo5_antpcr.png" },
-  { name: "Feast Palace", logo: "/logo/logo8_s3fz9x.png" },
-  { name: "Deli Divine", logo: "/logo/logo4_gir0vo.png" },
-  { name: "Grub lord", logo: "/logo/logo2_zzg8yb.png" },
-  { name: "Island Grill", logo: "/logo/logo3_zkcxdh.png" },
-  { name: "Pancake World", logo: "/logo/logo7_mbz4rt.png" },
-  { name: "Munchies", logo: "/logo/logo1_rvjrvm.png" },
-  { name: "Mediterra Seafood", logo: "/logo/logo6_rnywea.png" },
+  {
+    name: "Eatery works",
+    logo: "/image/upload/v1670077269/Mealy/logo/logo5_antpcr.png",
+  },
+  {
+    name: "Feast Palace",
+    logo: "image/upload/v1670077269/Mealy/logo/logo8_s3fz9x.png",
+  },
+  {
+    name: "Deli Divine",
+    logo: "/image/upload/v1670077268/Mealy/logo/logo4_gir0vo.png",
+  },
+  {
+    name: "Grub lord",
+    logo: "/image/upload/v1670077267/Mealy/logo/logo2_zzg8yb.png",
+  },
+  {
+    name: "Island Grill",
+    logo: "/image/upload/v1670077267/Mealy/logo/logo3_zkcxdh.png",
+  },
+  {
+    name: "Pancake World",
+    logo: "/image/upload/v1670077267/Mealy/logo/logo7_mbz4rt.png",
+  },
+  {
+    name: "Munchies",
+    logo: "/image/upload/v1670077267/Mealy/logo/logo6_rnywea.png",
+  },
+  {
+    name: "Mediterra Seafood",
+    logo: "/image/upload/v1670077267/Mealy/logo/logo1_rvjrvm.png",
+  },
 ]
 
 export const meal = [
@@ -16,7 +40,7 @@ export const meal = [
     type: "Food",
     description:
       "This infused arrowroot stew is a shade slimy with a crispy texture. It is balanced with chia with cicely and has a large amount of sorrel. It smells foul with limes. It is piquant and spicy. You can really feel how high in vitamin B and how all natural it is.",
-    image: "/food/timmy_pizza_nvj4yy.png",
+    image: "/image/upload/v1669934272/Mealy/food/timmy_pizza_nvj4yy.png",
   },
   {
     name: "Beefy Burger",
@@ -24,7 +48,8 @@ export const meal = [
     type: "Food",
     description:
       "This charred grapes eggroll is thoroughly honeyed with a delicate texture. It is similar to hemp seeds with cotula and has a touch of laurel. It smells fusty with tapioca. It is peculiarly grateful. You can really feel how locally sourced and how paleo-friendly it is.",
-    image: "/food/wepik-photo-mode-2022927-145744_llitra.png",
+    image:
+      "/image/upload/v1669934273/Mealy/food/wepik-photo-mode-2022927-145744_llitra.png",
   },
   {
     name: "Full Shrimps",
@@ -32,7 +57,7 @@ export const meal = [
     type: "Food",
     description:
       "This seared soybeans quiche is barely blackened with a crunchy texture. It resembles corn (maize) with garlic and has a great deal of chamomile. It smells like a cemetery with a bit of chrysanthemum leaves. It is decidedly literary. You can really feel how naturally sourced and how certified organic it is.",
-    image: "/food/full_shrimps_a5pupe.png",
+    image: "/image/upload/v1669934272/Mealy/food/full_shrimps_a5pupe.png",
   },
   {
     name: "Spagetti with Tomato Stew",
@@ -40,7 +65,8 @@ export const meal = [
     type: "Food",
     description:
       "This fermented butternut squash lasagna is a shade syrupy with a hearty texture. It has an undertone of kumquat with aralia and has an overwhelming amount of hibiscus. It smells savory with an overwhelming amount of melon balls. It is pleasantly seductive. You can really feel how high fiber and how free trade it is.",
-    image: "/food/spagetti_with_tomato_stew_evpyay.png",
+    image:
+      "/image/upload/v1669934271/Mealy/food/spagetti_with_tomato_stew_evpyay.png",
   },
   {
     name: "Jollof Rice with Beef",
@@ -48,7 +74,8 @@ export const meal = [
     type: "Food",
     description:
       "This sauteed acérola tapas is sort of dense with a silky texture. It bursts with the flavor of mulberries with southernwood and has a great deal of coriander-blair. It smells like licorice with just the right amount of prairie turnips. It is slightly scorched. You can really feel how no artificial colors and how vegan it is.",
-    image: "/food/jollof_rice_with_beef_kx2tde.png",
+    image:
+      "/image/upload/v1669934272/Mealy/food/jollof_rice_with_beef_kx2tde.png",
   },
   {
     name: "Lunch Combo",
@@ -56,7 +83,8 @@ export const meal = [
     type: "Food",
     description:
       "This breaded pistachios hummus is a touch fluffy with a gooey texture. It is enhanced by pearl millet with lemon balm and has cumin. It smells like cotton candy with a touch of sugar-apples. It is healthy and hearty. You can really feel how gluten free and how third party tested it is.",
-    image: "/food/wepik-photo-mode-2022927-145125_lonzng.png",
+    image:
+      "/image/upload/v1669934271/Mealy/food/wepik-photo-mode-2022927-145125_lonzng.png",
   },
   {
     name: "Macaroni Delight",
@@ -64,7 +92,8 @@ export const meal = [
     type: "Food",
     description:
       "This smoked iceberg lettuce eggroll is utterly juicy with a burned texture. It has an undertone of pomme with star anise and has a large amount of dill. It smells rank with a lot of peaches. It is vaguely Moorish. You can really feel how organic and how whole food it is.",
-    image: "/food/wepik-photo-mode-2022927-145916_b649ab.png",
+    image:
+      "/image/upload/v1669934270/Mealy/food/wepik-photo-mode-2022927-145916_b649ab.png",
   },
   {
     name: "Full Chicken With Stew",
@@ -72,7 +101,8 @@ export const meal = [
     type: "Food",
     description:
       "This fried sugar snap peas paella is sort of electric with a dense texture. It is infused with black-eyed peas with southernwood and has arnika. It smells like acetone with a dash of peanuts. It is bitter and acrid. You can really feel how high in calcium and how antioxidant rich it is.",
-    image: "/food/full_chicken_with_stew_lwe9so.png",
+    image:
+      "/image/upload/v1669934270/Mealy/food/full_chicken_with_stew_lwe9so.png",
   },
   {
     name: "Veggies Buffet",
@@ -80,7 +110,7 @@ export const meal = [
     type: "Food",
     description:
       "This sauteed broccoli lunch is a shade grainy with a silky texture. It builds depth and complexity with cannellini beans with black pepper and has a touch of cardamom. It smells like a hospital with cherimoya. It is metallic and medicinal. You can really feel how fresh cut and how nutrient rich it is.",
-    image: "/food/veggies_buffet_nxy2ep.png",
+    image: "/image/upload/v1669934270/Mealy/food/veggies_buffet_nxy2ep.png",
   },
   {
     name: "Pasta Frivoli",
@@ -88,7 +118,7 @@ export const meal = [
     type: "Food",
     description:
       "This whipped buckwheat quesadilla is quite blackened with an airy texture. It is redolent with blackberries with chamomile and has a hint of horseradish. It smells like perfume with pokeberry shoots. It is particularly new. You can really feel how high in probiotics and how antioxidant rich it is.",
-    image: "/food/pasta-frivoli_jssrig.png",
+    image: "/image/upload/v1669933261/Mealy/food/pasta-frivoli_jssrig.png",
   },
   {
     name: "Chocolate Pancakes",
@@ -96,7 +126,7 @@ export const meal = [
     type: "Food",
     description:
       "This blackened onion roast is a little sugary with a syrupy texture. It tastes strongly of sweet red pepper with mullein and has a dash of tarragon. It smells acrid with an overwhelming amount of nuts. It is strongly literary. You can really feel how high in omega 3s and how locally sourced it is.",
-    image: "/food/chocolate_pancake_njtvf1.png",
+    image: "/image/upload/v1669933261/Mealy/food/chocolate_pancake_njtvf1.png",
   },
   {
     name: "Seasoned Stake",
@@ -104,7 +134,7 @@ export const meal = [
     type: "Food",
     description:
       "This blackened finger millet taco is wholly mushy with a crispy texture. It has characteristics of olives with bee balm and has angelica. It smells ambrosial with a dash of butterbur. It is especially luscious. You can really feel how organic and how third party tested it is.",
-    image: "/food/seasoned_stake_lbskbo.png",
+    image: "/image/upload/v1669933260/Mealy/food/seasoned_stake_lbskbo.png",
   },
   {
     name: "Potato Soup",
@@ -112,7 +142,7 @@ export const meal = [
     type: "Food",
     description:
       "This charred guava brunch is sort of hearty with a gooey texture. It embodies the essence of wheat with savory and has a dash of cuban oregano. It smells like scented soap with a great deal of vermicelli. It is racy and delicious. You can really feel how high in omega 3s and how all natural it is.",
-    image: "/food/potato_soup_d8iekj.png",
+    image: "/image/upload/v1669933259/Mealy/food/potato_soup_d8iekj.png",
   },
   {
     name: "Pork Stew",
@@ -120,7 +150,7 @@ export const meal = [
     type: "Food",
     description:
       "This broiled Asian rice stew is scarcely mushy with a dense texture. It has a tinge of papaya with scullcap and has cinnamon. It smells like it is decomposing with a hint of balsam-pear (bitter gourd). It is faintly sweet. You can really feel how all natural and how high in magnesium it is.",
-    image: "/food/pork_stew_czgzuz.jpg",
+    image: "/image/upload/v1669933259/Mealy/food/pork_stew_czgzuz.jpg",
   },
   {
     name: "Bread and Egg Breakfast",
@@ -128,7 +158,8 @@ export const meal = [
     type: "Food",
     description:
       "This fermented quinoa dessert is altogether hearty with a succulent texture. It is similar to sorana beans with rosemary and has a dash of bayberry. It smells delicate with a dash of fireweed. It is syrupy sweet. You can really feel how high in probiotics and how high in iron it is.",
-    image: "/food/bread_and_egg_breakfast_hw78sy.png",
+    image:
+      "/image/upload/v1669933258/Mealy/food/bread_and_egg_breakfast_hw78sy.png",
   },
   {
     name: "Dumplins",
@@ -136,7 +167,7 @@ export const meal = [
     type: "Food",
     description:
       "This breaded mangosteen eggroll is absolutely intoxicating with a sticky texture. It has an undertone of carrot with paprika and has mugwort. It smells like mold with a lot of jew's ear. It is has variety. You can really feel how premium and how high in riboflavin it is",
-    image: "/food/dumplins_w5rqdj.jpg",
+    image: "/image/upload/v1669933257/Mealy/food/dumplins_w5rqdj.jpg",
   },
   {
     name: "Noodles and Shrimp",
@@ -144,7 +175,7 @@ export const meal = [
     type: "Food",
     description:
       "This breaded Asian rice wrap is comparatively honeyed with a buttery texture. It compares to pomme with hyssop and has a touch of rue. It smells like chicken broth with a lot of melon balls. It is slightly minerally. You can really feel how paleo-friendly and how high in probiotics it is.",
-    image: "/food/noodles_and_shrimp_vothmr.jpg",
+    image: "/image/upload/v1669822848/Mealy/food/noodles_and_shrimp_vothmr.jpg",
   },
   {
     name: "Freevolies",
@@ -152,7 +183,7 @@ export const meal = [
     type: "Food",
     description:
       "This blanched banana appetizer is a little velvety with a fizzy texture. It has characteristics of finger millet with nettle and has just the right amount of celery seed. It smells clean with a large amount of ginger root. It is truly German. You can really feel how high in omega 3s and how fresh cut it is.",
-    image: "/food/freevolies_lrdxtw.jpg",
+    image: "/image/upload/v1669933258/Mealy/food/freevolies_lrdxtw.jpg",
   },
   {
     name: "Grilled Ribs",
@@ -160,7 +191,7 @@ export const meal = [
     type: "Food",
     description:
       "This glazed apple wrap is totally succulent with a tough texture. It is redolent with goji berries with mace and has an overwhelming amount of dill. It smells squalid with a tiny bit of longans. It is pleasantly sweet. You can really feel how third party tested and how high in healthy fats it is.",
-    image: "/food/grilled_ribs_djpixw.jpg",
+    image: "/image/upload/v1669822847/Mealy/food/grilled_ribs_djpixw.jpg",
   },
   {
     name: "Meat and Veggie Bowl",
@@ -168,7 +199,8 @@ export const meal = [
     type: "Food",
     description:
       "This marinated black-eyed peas sushi is fairly soft with an intoxicating texture. It is infused with adzuki beans with mullein and has just the right amount of arnika. It smells like maple sugar with just the right amount of plums. It is acrid and pungent. You can really feel how fresh cut and how soy free it is.",
-    image: "/food/meat_and_veggie_bowl_ety9di.jpg",
+    image:
+      "/image/upload/v1669822847/Mealy/food/meat_and_veggie_bowl_ety9di.jpg",
   },
 ]
 
@@ -178,76 +210,77 @@ export const drink = [
     price: 18.0,
     type: "Drink",
     description: "A very rich in content chocolate milkshake",
-    image: "/drink/chocolate_milkshake_m3omy9.png",
+    image:
+      "/image/upload/v1669935762/Mealy/drink/chocolate_milkshake_m3omy9.png",
   },
   {
     name: "Lemonade",
     price: 10.0,
     type: "Drink",
     description: "Lemonade drink made with 100% lemon",
-    image: "/drink/lemonade_b2n3bp.png",
+    image: "/image/upload/v1669935760/Mealy/drink/lemonade_b2n3bp.png",
   },
   {
     name: "Creme crame",
     price: 15.0,
     type: "Drink",
     description: "Cold creammy malt",
-    image: "/drink/creme_crame_bfwcpz.png",
+    image: "/image/upload/v1669935758/Mealy/drink/creme_crame_bfwcpz.png",
   },
   {
     name: "Coca cola",
     price: 13.0,
     type: "Drink",
     description: "A coca cola drink",
-    image: "/drink/coca_cola_g7alx5.png",
+    image: "/image/upload/v1669935758/Mealy/drink/coca_cola_g7alx5.png",
   },
   {
     name: "Coffee",
     price: 15.0,
     type: "Drink",
     description: "Coffee drink to boost energy",
-    image: "/drink/coffee_vbwmse.png",
+    image: "/image/upload/v1669935760/Mealy/drink/lemonade_b2n3bp.png",
   },
   {
     name: "Iced Tea",
     price: 16.0,
     type: "Drink",
     description: "Chilled ice tea",
-    image: "/drink/iced_tea_wjzfi8.png",
+    image: "/image/upload/v1669935756/Mealy/drink/iced_tea_wjzfi8.png",
   },
   {
     name: "Orange Juice",
     price: 20.0,
     type: "Drink",
     description: "Freshly squeezed orange juice",
-    image: "/drink/orange_juice_kxaubc.png",
+    image: "/image/upload/v1669935756/Mealy/drink/orange_juice_kxaubc.png",
   },
   {
     name: "Chupa Chups",
     price: 12.0,
     type: "Drink",
     description: "Chupa chubs flavored drink",
-    image: "/drink/chupa_chups_xfezk5.png",
+    image: "/image/upload/v1669935755/Mealy/drink/chupa_chups_xfezk5.png",
   },
   {
     name: "Mundet",
     price: 15.0,
     type: "Drink",
     description: "Cool mundet drink",
-    image: "/drink/mundet_bk6ed9.png",
+    image: "/image/upload/v1669935755/Mealy/drink/mundet_bk6ed9.png",
   },
   {
     name: "Fanta",
     price: 14.0,
     type: "Drink",
     description: "Chilled fanta drink",
-    image: "/drink/fanta_gick5i.png",
+    image: "/image/upload/v1669935755/Mealy/drink/fanta_gick5i.png",
   },
   {
     name: "Tomato Juice",
     price: 11.0,
     type: "Drink",
     description: "Freshly squeezed tomato",
-    image: "/drink/tomato_juice_wg7cfn.jpg",
+    image: "/image/upload/v1669935755/Mealy/drink/fanta_gick5i.png",
   },
 ]
